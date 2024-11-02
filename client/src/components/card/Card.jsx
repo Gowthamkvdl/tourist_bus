2f7b8b6be1aa0b8ddf5dd766699b9379515e9e7a
gowthamkvdl <gowthamkvdl@gmail.com>
2024-11-02 11:27:55 +0530

    dvasdv

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -52,7 +52,7 @@ const Card = () => {
           </div>
           <div className="col-5 d-flex ">
             <div className="btn primary-600 rounded-5 m-auto">
-              <Link to={"/info/sghe"}>
+              <Link to={"/info/sghe"} className="link">
                 <div className="d-flex align-items-center">
                   <span className="body-text fw-semibold text-white ">More Info</span>
                   <span class="material-symbols-outlined text-white">
