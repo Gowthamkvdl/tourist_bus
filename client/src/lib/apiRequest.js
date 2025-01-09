008a9f29841b3338f61a5f7f8617823634d2f259
gowthamkvdl <gowthamkvdl@gmail.com>
2025-01-09 09:38:24 +0530

    ghjghk

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:3000/api",
-  withCredentials: true,
-
-  // baseURL: "https://tourist-bus-backend.onrender.com/api",
+  // baseURL: "http://localhost:3000/api",
   // withCredentials: true,
+
+  baseURL: "https://tourist-bus-backend.onrender.com/api",
+  withCredentials: true,
 });   
 
 export default apiRequest;
