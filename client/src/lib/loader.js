2b763731d84a906c68a5a74a2eb0a1606cec5c11
Gowtham <gowthamkvdl@gmail.com>
2025-02-21 23:02:45 -0800

    rthrth

--- client/src/lib/loader.js
+++ client/src/lib/loader.js
@@ -1,7 +1,6 @@
 import { defer } from "react-router-dom";
 import apiRequest from "./apiRequest";
 
-
 export const infoPageLoader = async ({ params }) => {
   return { id: params.id }; // Only passing the ID
 };
@@ -19,13 +18,14 @@ export const editPageLoader = async ({ params }) => {
 };
 
 export const listPageLoader = ({ request }) => {
-  const [others, limitStr] = request.url.split("?")[1].split("&limit=");
+  const others = request.url.split("?")[1] ? request.url.split("?")[1] : "";
+  const limitStr = request.url.split("&limit=")
+    ? request.url.split("&limit=")
+    : "";
   const limit = parseInt(limitStr);
-  const postPromise =  apiRequest.get(`/post/posts?${others}&limit=${limit}`);
+  const postPromise = apiRequest.get(`/post/posts?${others}&limit=${limit}`);
   return defer({
     postResponse: postPromise,
   });
   // return postPromise.data;
 };
-
-
