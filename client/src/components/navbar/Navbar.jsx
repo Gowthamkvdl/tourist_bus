95315b138097c19e18a8fd48b98bd28ffef64ce3
Gowtham <gowthamkvdl@gmail.com>
2025-02-22 23:48:55 -0800

    jhjkbjk

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -271,7 +271,7 @@ const Navbar = () => {
         </div>
         <div>
           <div
-            className="collapse primary-400  mb-4 mb-md-0  navbar-collapse box-shadow px-3 rounded-4"
+            className="collapse primary-400  mb-2 mb-md-0  navbar-collapse box-shadow px-3 rounded-4"
             id="navbarNav"
           >
             <ul className="navbar-nav">
