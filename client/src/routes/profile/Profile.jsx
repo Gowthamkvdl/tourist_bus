a0615e72a40f1bf57c8b69666a779402a85acb22
Gowtham K <gowthamkvdl@gmail.com>
2025-04-21 07:54:59 -0700

    iuyhyui

--- client/src/routes/profile/Profile.jsx
+++ client/src/routes/profile/Profile.jsx
@@ -431,16 +431,15 @@ const Profile = () => {
           {data?.postData?.filter(
             (post) =>
               post.verificationStatus === "rejected" ||
-              post.verificationStatus === "pending"
+              post.verificationStatus === "pending" &&
+              post.hasImage === true 
           ).length > 0 && (
             <h1 className="title-text pt-3 pb-1 opacity-75">
               Your Buses Under Verification Process
             </h1>
           )}
-          {isLoading ? (
-            // Show loading skeleton while fetching data
-            null
-          ) : error ? (
+          {isLoading ? // Show loading skeleton while fetching data
+          null : error ? (
             // Show error message if API request fails
             <p className="text-center">
               <ErrorComponent />
@@ -466,18 +465,17 @@ const Profile = () => {
                   </button> */}
                 </div>
               ))
-          ) : (
-            null
-            // Fallback message
-            // data?.postData?.filter(
-            //   (post) => !post.verificationStatus === "accepted"
-            // ).length > 0 && (
-            //   <p className="opacity-75">
-            //     You haven't submited any buses to verify yet. Start uploading
-            //     now!
-            //   </p>
-            // )
-          )}
+          ) : null
+          // Fallback message
+          // data?.postData?.filter(
+          //   (post) => !post.verificationStatus === "accepted"
+          // ).length > 0 && (
+          //   <p className="opacity-75">
+          //     You haven't submited any buses to verify yet. Start uploading
+          //     now!
+          //   </p>
+          // )
+          }
 
           {data?.postData?.filter((post) => !post.hasImage).length > 0 && (
             <h1 className="title-text pt-3 pb-1 opacity-75">
@@ -522,15 +520,14 @@ const Profile = () => {
                   </button>
                 </div>
               ))
-          ) : (
-            // Fallback message
-            null
-            // data.postData.filter((post) => !post.hasImage).length > 0 && (
-            //   <p className="opacity-75">
-            //     No unfinished bus uploads yet. Start uploading to see them here!
-            //   </p>
-            // )
-          )}
+          ) : // Fallback message
+          null
+          // data.postData.filter((post) => !post.hasImage).length > 0 && (
+          //   <p className="opacity-75">
+          //     No unfinished bus uploads yet. Start uploading to see them here!
+          //   </p>
+          // )
+          }
 
           <h1 className="title-text pb-1 mt-3 opacity-75">Your Buses</h1>
           {isLoading ? (
@@ -554,28 +551,44 @@ const Profile = () => {
               .map((post) => (
                 <div className="col-md-6" key={post.postId}>
                   <Card post={post} />
-                  <button
-                    className="btn btn-warning me-2 mb-4 d-flex justify-content-center align-items-center"
-                    onClick={() => handleEditClick(post.postId)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="currentColor"
-                      class="bi bi-pencil-square me-1"
-                      viewBox="0 0 16 16"
+                  <div className="btns d-flex">
+                    <button
+                      className="btn btn-warning me-2 mb-4 d-flex justify-content-center align-items-center"
+                      onClick={() => handleEditClick(post.postId)}
                     >
-                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                      <path
-                        fill-rule="evenodd"
-                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-                      />
-                    </svg>
-                    <div>
-                      {editLoading[post.postId] ? "Loading..." : "Edit Bus"}
-                    </div>
-                  </button>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-pencil-square me-1"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                        <path
+                          fill-rule="evenodd"
+                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                        />
+                      </svg>
+                      <div>
+                        {editLoading[post.postId] ? "Loading..." : "Edit Bus"}
+                      </div>
+                    </button>
+                    <button className="btn btn-info me-2 mb-4 d-flex justify-content-center align-items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="bi bi-eye me-1"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                      </svg>
+                      <div>{post.views}</div>
+                    </button>
+                  </div>
                 </div>
               ))
           ) : (
