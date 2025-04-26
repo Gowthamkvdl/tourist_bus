6b291fd4afc5acdcdcd2a69bf1420070a35d0aa5
Gowtham K <gowthamkvdl@gmail.com>
2025-04-26 08:57:39 -0700

    gfhfhy

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  // baseURL: "https://tourist-bus-backend.onrender.com/api",
-  // withCredentials: true,
-
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://tourist-bus-backend.onrender.com/api",
   withCredentials: true,
+
+  // baseURL: "http://localhost:3000/api",
+  // withCredentials: true,
 });   
 
 export default apiRequest;
