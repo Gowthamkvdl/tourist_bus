c6f81d7b1c039534d3a0236cd8fd8017d6303c20
Gowtham <gowtham@gmail.com>
2026-04-02 16:15:36 +0530

    feat: redesign InfoSkeleton component for enhanced loading experience with additional sections

--- client/src/components/infoSkeleton/InfoSkeleton.jsx
+++ client/src/components/infoSkeleton/InfoSkeleton.jsx
@@ -1,124 +1,84 @@
-import React from "react";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-import BackBtn from "../backBtn/BackBtn";
+<div className="container mt-3">
+  <div className="row g-3">
 
-const InfoSkeleton = () => {
-  return (
-    <div>
-      <div className="header mb-3 pt-md-5">
-        <div className="title-text text-center opacity-75 mt-3">
-          <BackBtn />
-          <span className="text-center">Bus Details</span>
+    {/* LEFT IMAGE */}
+    <div className="col-md-6">
+      <Skeleton height={250} className="rounded-3" />
+    </div>
+
+    {/* RIGHT DETAILS */}
+    <div className="col-md-6 d-flex flex-column gap-2">
+
+      {/* title + heart */}
+      <div className="d-flex justify-content-between align-items-start">
+        <div>
+          <Skeleton width={180} height={20} />
+          <Skeleton width={120} height={12} />
+          <Skeleton width={100} height={15} />
         </div>
+        <Skeleton circle width={30} height={30} />
       </div>
-      <div className="others box-shadow pt-1 pb-5 bg-white">
-        <div className="busname mt-3 d-flex justify-content-between align-items-center">
-          <div className="d-flex mx-auto justify-content-center align-items-center gap-1">
-            <div className="nameAndRating d-flex flex-column justify-content-center align-items-center">
-              <Skeleton width={200} height={20} />
-              <div className="location subtitle-text mb-1 d-flex justify-content-center align-items-center">
-                <Skeleton width={150} height={25} />
-              </div>
-              <div className="stars d-flex justify-content-center align-items-center">
-                <Skeleton width={100} height={20} />
-              </div>
-            </div>
-          </div>
-          <Skeleton circle width={40} height={40} className="me-4" />
-        </div>
 
-        <div className="imgs mx-3 w-md-75 my-3 rounded-3">
-          <Skeleton height={200} />
+      {/* specs table */}
+      {Array(6).fill(0).map((_, i) => (
+        <div key={i} className="d-flex justify-content-between">
+          <Skeleton width={120} height={15} />
+          <Skeleton width={60} height={15} />
         </div>
+      ))}
 
-        <div className="details mx-md-4 px-4">
-          <div className="Price d-flex justify-content-between align-items-center">
-            <Skeleton height={40} />
-            <Skeleton height={40} />
-          </div>
-          <div className="contact mt-3">
-            <div className="row">
-              <div className="col-6">
-                <Skeleton height={40} />
-              </div>
-              <div className="col-6">
-                <Skeleton height={40} />
-              </div>
-            </div>     
-            <hr />
-          </div>
-          <div className="more"> 
-            <div className="subtitle-text opacity-75 mb-3">
-              <Skeleton width={150} height={30} />
-            </div>
-            <div className="type d-flex justify-content-between align-items-center mb-2">
-              <Skeleton width={100} height={20} />
-              <Skeleton width={50} height={20} />
-            </div>
-            <div className="type d-flex justify-content-between align-items-center mb-2">
-              <Skeleton width={100} height={20} />
-              <Skeleton width={50} height={20} />
-            </div>
-            <div className="type d-flex justify-content-between align-items-center mb-2">
-              <Skeleton width={100} height={20} />
-              <Skeleton width={50} height={20} />
-            </div>
-            <div className="type d-flex justify-content-between align-items-center mb-2">
-              <Skeleton width={100} height={20} />
-              <Skeleton width={50} height={20} />
-            </div>
-            <div className="type mb-3 d-flex justify-content-between align-items-center">
-              <Skeleton width={100} height={20} />
-              <Skeleton width={50} height={20} />
-            </div>
-            <div className="subtitle-text opacity-75 mb-3">
-              <Skeleton width={150} height={30} />
-            </div>
-            <div className="type d-flex justify-content-between align-items-center mb-2">
-              <Skeleton width={100} height={20} />
-              <Skeleton width={50} height={20} />
-            </div>
-            <div className="type d-flex justify-content-between align-items-center mb-2">
-              <Skeleton width={100} height={20} />
-              <Skeleton width={50} height={20} />
-            </div>
-            <div className="type d-flex justify-content-between align-items-center mb-2">
-              <Skeleton width={100} height={20} />
-              <Skeleton width={50} height={20} />
-            </div>
-          </div>
-          <hr />
-          <div className="reviews">
-            <div className="headingAndStars d-flex justify-content-between">
-              <Skeleton width={150} height={30} />
-              <Skeleton width={100} height={30} />
-            </div>
-            <div className="review mb-5">
-              <div className="reviewBox d-flex mt-4">
-                <div className="photoDiv d-flex">
-                  <Skeleton circle width={40} height={40} />
-                </div>
-                <div className="nameAndReview ms-2">
-                  <Skeleton width={150} height={20} />
-                  <Skeleton width={200} height={20} />
-                </div>
-              </div>
-              <div className="reviewBox d-flex mt-4">
-                <div className="photoDiv d-flex">
-                  <Skeleton circle width={40} height={40} />
-                </div>
-                <div className="nameAndReview ms-2">
-                  <Skeleton width={150} height={20} />
-                  <Skeleton width={200} height={20} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      {/* price */}
+      <div className="mt-2">
+        <Skeleton width={100} height={25} />
+      </div>
+
+      {/* buttons */}
+      <div className="d-flex gap-2 mt-2">
+        <Skeleton height={40} width={120} borderRadius={8} />
+        <Skeleton height={40} width={120} borderRadius={8} />
       </div>
     </div>
-  );
-};
+  </div>
 
-export default InfoSkeleton;
+  {/* ABOUT */}
+  <div className="mt-4">
+    <Skeleton width={150} height={20} />
+    <Skeleton width={300} height={15} />
+  </div>
+
+  {/* AMENITIES */}
+  <div className="mt-3 d-flex gap-2 flex-wrap">
+    {Array(5).fill(0).map((_, i) => (
+      <Skeleton key={i} width={70} height={25} borderRadius={20} />
+    ))}
+  </div>
+
+  {/* ENTERTAINMENT */}
+  <div className="mt-4">
+    <Skeleton width={150} height={20} />
+    {Array(3).fill(0).map((_, i) => (
+      <div key={i} className="d-flex justify-content-between mt-2">
+        <Skeleton width={120} height={15} />
+        <Skeleton width={80} height={15} />
+      </div>
+    ))}
+  </div>
+
+  {/* REVIEWS */}
+  <div className="mt-4">
+    <div className="d-flex justify-content-between">
+      <Skeleton width={150} height={20} />
+      <Skeleton width={100} height={30} />
+    </div>
+
+    {Array(2).fill(0).map((_, i) => (
+      <div key={i} className="d-flex gap-2 mt-3">
+        <Skeleton circle width={40} height={40} />
+        <div>
+          <Skeleton width={120} height={15} />
+          <Skeleton width={200} height={15} />
+        </div>
+      </div>
+    ))}
+  </div>
+</div>
